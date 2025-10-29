67bf4ed3bf103ba065eab300d69b70d5f6b2f020
Tim <dev.atobatele@gmail.com>
2025-10-29 02:46:33 -0400

    Revert "Update main.py - origins"
    This reverts commit 9bdb8076168642f0284deff7d1fcd36892b5d6ed.
    
    Rollback to successful build

--- main.py
+++ main.py
@@ -15,11 +15,10 @@ from auth import (
 app = FastAPI(title="Social Media API (Auth)", version="1.3")
 Base.metadata.create_all(bind=engine)
 from fastapi.middleware.cors import CORSMiddleware
-import os
-origins = [os.getenv("FRONTEND_URL", "http://localhost:3000")]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
